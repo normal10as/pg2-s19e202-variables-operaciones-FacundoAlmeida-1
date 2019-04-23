c4f4dd59747c6c9bc96182a438351696a8e1e1e2
facundoa.almeida@outlook.es <facundoa.almeida@outlook.es>
2019-04-22 21:46:31 -0300

    .

--- Variables/Ecuacion/Program.vb
+++ Variables/Ecuacion/Program.vb
@@ -6,7 +6,7 @@ Module Program
         Dim A As SByte = 1
         Dim B As SByte = 5
         Dim C As SByte = 2
-        Console.WriteLine("x= " & B ^ 2 - 4 * A * C / 2 * A)
+        Console.WriteLine("x= " & (B ^ 2 - 4 * A * C) / 2 * A)
 
 
     End Sub
