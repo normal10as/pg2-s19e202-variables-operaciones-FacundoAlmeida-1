7f4ebcc5250452776e460262578156f86c8b15a0
facundoa.almeida@outlook.es <facundoa.almeida@outlook.es>
2019-05-09 01:13:58 -0300

    .

--- Variables/Semanas/Program.vb
+++ Variables/Semanas/Program.vb
@@ -5,6 +5,8 @@ Module Program
         Const dias As Integer = 365
         Const Dias_Laborables As Integer = 177
         Const Dias_semana As Integer = 7
+        Console.WriteLine(".")
+
 
         Console.WriteLine("El año tiene " & dias / Dias_semana & " Semanas. Y las semanas labolares son: " & Dias_Laborables / Dias_semana)
     End Sub
